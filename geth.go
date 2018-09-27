10a1a679fd87e983c7ff846b7ce42b53135a721d
lizekun <soap@easyspark.cn>
2018-09-27 11:45:44 +0800

    sendRawTransaction

--- geth.go
+++ geth.go
@@ -35,12 +35,13 @@ func (c *Client) GasPrice() (price *big.Int, err error) {
 	return
 }
 
-func (c *Client) EstimateGas(from, to string, value, gasPrice *big.Int) (limit uint64, err error) {
+func (c *Client) EstimateGas(from, to string, value, gasPrice *big.Int, data []byte) (limit uint64, err error) {
 	param := M{
 		"value":    value.Text(16),
 		"to":       to,
 		"form":     from,
 		"gasPrice": gasPrice.Text(16),
+		"data":     string(data),
 	}
 	limitStr := ""
 	err = c.call("proxy", "eth_estimateGas", param, &limitStr)
